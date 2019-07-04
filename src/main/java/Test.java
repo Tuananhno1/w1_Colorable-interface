1f14ddb19f2dcb1348f1ec788945768b5f996faa
Tuananhno1 <anhntd14ptit@gmail.com>
2019-07-04 10:56:23 +0700

    c1

---
+++ src/main/java/Test.java
@@ -0,0 +1,18 @@
+public class Test {
+    public static void main(String[] args) {
+        Shape[] shapes = new Shape[3];
+        shapes[0] =  new Circle(2.1);
+        shapes[1] =  new Rectangle(3.5, 2.1);
+        shapes[2] =  new square(1.2);
+
+        for (Shape shape :shapes){
+
+            if (shape instanceof square){
+                ((square) shape).howtocolor();
+            }
+            else {
+                System.out.println(shape.getArea());
+            }
+        }
+    }
+}
